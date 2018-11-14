f187bb7ab5c7ff3791d281378b80d8c05624d4ed
Ruben Hasas <ruben.hasas94@gmail.com>
2018-11-14 17:58:08 +0200

    Initial commit

--- src/main/java/com/virtuallibrary/repositories/BookRepository.java
+++ src/main/java/com/virtuallibrary/repositories/BookRepository.java
@@ -4,4 +4,6 @@ import com.virtuallibrary.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
+
+    Book findByTitle (String title);
 }
--- src/main/java/com/virtuallibrary/services/springdatajpa/BookSDJpaService.java
+++ src/main/java/com/virtuallibrary/services/springdatajpa/BookSDJpaService.java
@@ -1,4 +1,51 @@
 package com.virtuallibrary.services.springdatajpa;
 
-public class BookSDJpaService {
+import com.virtuallibrary.model.Book;
+import com.virtuallibrary.repositories.BookRepository;
+import com.virtuallibrary.services.BookService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class BookSDJpaService implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookSDJpaService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book findByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+    @Override
+    public Set<Book> findAll() {
+        Set<Book> books = new HashSet<>();
+        bookRepository.findAll().forEach(books::add);
+        return books;
+    }
+
+    @Override
+    public Book findById(Long aLong) {
+        return bookRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Book save(Book object) {
+        return bookRepository.save(object);
+    }
+
+    @Override
+    public void delete(Book object) {
+        bookRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        bookRepository.deleteById(aLong);
+    }
 }
